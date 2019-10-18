4a81490ee2dda4ff42f94e47d766957a0aab0ba2
MGraf <info@moritz-graf.de>
2019-10-18 15:30:31 +0200

    Add intro path and fetch categories

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import handlebars from 'handlebars';
 
-function fetchQuestions() {
+function fetchCategories() {
    const request =  new XMLHttpRequest();
-   request.open ("GET", "https://opentdb.com/api.php?amount=20&category=9&difficulty=medium&type=multiple", false); 
+   request.open ("GET", "https://opentdb.com/api_category.php", false); 
+   request.send();
+   // Do something, if the server doesn't respond
+   return JSON.parse(request.response).trivia_categories;
+}
+
+function fetchQuestions(categoryID) {
+   const request =  new XMLHttpRequest();
+   request.open ("GET", "https://opentdb.com/api.php?amount=20&category=" + categoryID + "&difficulty=medium&type=multiple", false); 
    request.send();
    // Do something, if the server doesn't respond
    return JSON.parse(request.response).results;
@@ -24,16 +32,35 @@ function getChoices (question) {
    return randomArray ([...question.incorrect_answers, question.correct_answer]);
 }
 
-const questions = fetchQuestions();
+var questions = [];
 
 const app = document.getElementById ('app');
 
 const questionTemplate = handlebars.compile (document.getElementById('question-template').innerHTML);
 const answerTemplate = handlebars.compile (document.getElementById('answer-template').innerHTML);
+const introTemplate = handlebars.compile (document.getElementById('intro-template').innerHTML);
 
 const routes = {
+   'intro' : {
+      render : function () {
+         return introTemplate ({
+            categories: fetchCategories()
+         })
+      },
+      script : function () {
+         document.getElementById ('start-button').addEventListener ('click', function (e) {
+            e.preventDefault();
+
+            questions = fetchQuestions(document.getElementById ('category-select').value);
+            console.log(questions);
+
+            navigate ('question', {index: 0});
+         })
+      }
+   },
    'question' : {
       render : function ({index}) {
+         console.log(questions);
          const question = questions[index];
          return questionTemplate ({
             question: question.question,
@@ -70,4 +97,4 @@ function navigate (path, params = {}) {
    route.script(params);
 }
 
-navigate ('question', {index: 0});
+navigate ('intro');
